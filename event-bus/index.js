8b0dec410b7292930967d0b91e6a27e17e135c39
seunope <seunope2004@yahoo.co.uk>
2022-10-08 14:34:29 +0100

    moderation event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-  const events = req.body;
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-  axios.post('http://localhost:4000/events', events).catch((err) => {
-    console.log('Post service', err);
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
   });
-
-  axios.post('http://localhost:4001/events', events).catch((err) => {
-    console.log('Comment service', err);
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
   });
-
-  axios.post('http://localhost:4002/events', events).catch((err) => {
-    console.log('Query service', err);
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
   });
-
-  res.status(201).send({ status: 'OK' });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-  console.log('listening on 4005');
+  console.log("Listening on 4005");
 });
